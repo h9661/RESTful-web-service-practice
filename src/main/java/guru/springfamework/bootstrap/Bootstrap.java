fc099f1847e4ccdb5dff279eed88c0eb32920f21
h9661 <guscksdn123@gmail.com>
2023-02-21 15:13:02 +0900

    added customer to bootstrap.

--- src/main/java/guru/springfamework/bootstrap/Bootstrap.java
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -1,18 +1,21 @@
 package guru.springfamework.bootstrap;
 
 import guru.springfamework.domain.Category;
+import guru.springfamework.domain.Customer;
 import guru.springfamework.repositories.CategoryRepository;
+import guru.springfamework.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
     private CategoryRepository categoryRepository;
+    private CustomerRepository customerRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
         this.categoryRepository = categoryRepository;
+        this.customerRepository = customerRepository;
     }
-
 
     @Override
     public void run(String... args) throws Exception {
@@ -37,6 +40,18 @@ public class Bootstrap implements CommandLineRunner {
         categoryRepository.save(exotic);
         categoryRepository.save(nuts);
 
-        System.out.println("Data Loaded = " + categoryRepository.count());
+        Customer customer1 = new Customer(1L);
+        customer1.setFirstName("hyun");
+        customer1.setLastName("woo");
+
+        Customer customer2 = new Customer(2L);
+        customer2.setFirstName("jon");
+        customer2.setLastName("Tom");
+
+        customerRepository.save(customer1);
+        customerRepository.save(customer2);
+
+        System.out.println("Data Loaded Category = " + categoryRepository.count());
+        System.out.println("Data Loaded Customer = " + customerRepository.count());
     }
 }
